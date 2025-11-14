8825bdc8e9b81991a59d441663b1443a81f9870d
NandhiEsswaran-001 <nandhiesswaransaravanan@gmail.com>
2025-11-14 20:37:33 +0530

    1st commit finished with the workout page section ui

---
+++ src/components/Grid.vue
@@ -0,0 +1,55 @@
+<script setup>
+    import { workoutProgram } from '../utils';
+
+    const workoutTypes = ['Push', 'Pull', 'Legs']
+</script>
+
+<template>
+    <section id="grid">
+        <button :key="workoutIds" v-for="(workout, workoutIds) in Object.keys(workoutProgram)" class="card-button plan-card">
+            <div>
+                <p>Day {{ workoutIds < 9 ? `0${workoutIds + 1}` : workoutIds + 1 }}</p>
+                <i class="fa-solid fa-dumbbell" v-if="workoutIds % 3 == 0"></i>
+                <i class="fa-solid fa-weight-hanging" v-if="workoutIds % 3 == 1"></i>
+                <i class="fa-solid fa-bolt" v-if="workoutIds % 3 == 2"></i>
+            </div>
+            <h3>
+                {{ workoutTypes[workoutIds % 3] }}
+            </h3>
+        </button>
+    </section>
+</template>
+
+<style scoped>
+    #grid {
+        display: grid;
+        grid-template-columns: repeat(3, minmax(0, 1fr));
+        gap: 1rem;
+    }
+
+    #grid button {
+        width: 100%;
+    }
+
+    .plan-card {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .plan-card div {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        gap: 0.5rem;
+    }
+
+    .plan-card div p {
+        text-align: left;
+    }
+
+    @media (min-width: 640px) {
+        #grid {
+            grid-template-columns: repeat(4, minmax(0, 1fr));
+        }
+    }
+</style>
---
+++ src/components/pages/Dashboard.vue
@@ -0,0 +1,54 @@
+<script setup>
+    import { gymHealthFacts } from '../../utils';
+    import Grid from "../Grid.vue"
+
+    // generating random numbers
+    const randomNumber = Math.floor(Math.random() * gymHealthFacts.length)
+    const todayFacts = gymHealthFacts[randomNumber]
+
+    
+</script>
+
+<template>
+    <section id="dashboard">
+        <div class="card tip-container">
+            <h2>Welcome Fitters</h2>
+            <div>
+                <p class="tip">
+                    <strong>Daily Tip</strong> <br> {{ todayFacts }}
+                </p>
+            </div>
+
+            <button>Start Workout &rarr;</button>
+        </div>
+
+        <Grid />
+    </section>
+</template>
+
+<style scoped>
+    .tip-container,
+    .tip-container div,
+    #dashboard {
+        display: flex;
+    }
+
+    .tip-container,
+    #dashboard {
+        flex-direction: column;
+    }
+
+    #dashboard {
+        gap: 2rem;
+    }
+
+    .tip-container {
+        gap: 0.5rem;
+    }
+
+    @media (min-width: 640px) {
+        .tip-container {
+            gap: 1rem;
+        }
+    }
+</style>
